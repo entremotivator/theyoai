3594e4d2be91db9e2c1f2f2078c6b36b2ed0f07d
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-25 12:39:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
-# Allow users to enter their own server info in the sidebar
 import streamlit as st
 import openai
 import json
 import os
 from datetime import datetime
 
-# Default OpenAI API settings
-openai.api_base = 'http://localhost:1234/v1'
-openai.api_key = ''
-
 # Function to configure OpenAI API settings
 def configure_openai_api():
     st.sidebar.subheader("OpenAI API Settings")
-    openai.api_base = st.sidebar.text_input("API Base URL", openai.api_base)
-    openai.api_key = st.sidebar.text_input("API Key", openai.api_key, type="password")
+    api_base = st.sidebar.text_input("API Base URL", "http://localhost:1234/v1")
+    api_key = st.sidebar.text_input("API Key", type="password")
+    return api_base, api_key
 
-# Your function for retrieving completions
-def get_completion(prompt, model="local model", temperature=0.0):
-    # (unchanged code)
+# Function to get OpenAI completion
+def get_completion(prompt, model="local model", temperature=0.0, api_base='http://localhost:1234/v1', api_key=''):
+    openai.api_base = api_base
+    openai.api_key = api_key
 
-# Function for the session list
-def save_session_index(session_key):
-    # (unchanged code)
+    prefix = "### Instruction:\n"
+    suffix = "\n### Response:"
+    formatted_prompt = f"{prefix}{prompt}{suffix}"
+    messages = [{"role": "user", "content": formatted_prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature
+    )
+    return response.choices[0].message["content"]
 
+# Rest of the code remains the same...
+
+# Main app function
 def app():
     st.title("Jarvis Chatbot")
     os.makedirs('chat_data', exist_ok=True)
 
     # Configure OpenAI API settings
-    configure_openai_api()
+    api_base, api_key = configure_openai_api()
 
     # Sidebar
     st.sidebar.title("New Chat")
@@ -43,16 +50,24 @@ def app():
     st.sidebar.header("Chat History")  # Header
 
     # List of existing chats
-    # (unchanged code)
+    try:
+        with open(os.path.join('chat_data', 'session_index.json'), 'r') as f:
+            session_index = json.load(f)
+    except FileNotFoundError:
+        session_index = []
+    
+    for session in session_index:
+        if st.sidebar.button(session["name"]):
+            st.session_state['session_key'] = session["name"]
 
     # Load chat history
     if 'session_key' in st.session_state:
         st.header(st.session_state['session_key'])  
         chat_history = load_chat_history(st.session_state['session_key'])
         
-        user_input = st.chat_input("You:")
+        user_input = st.text_input("You:")
         if user_input:
-            response = get_completion(user_input)
+            response = get_completion(user_input, api_base=api_base, api_key=api_key)
             chat_history.append({"role": "user", "message": user_input})
             chat_history.append({"role": "Jarvis", "message": response})
             save_chat_history(st.session_state['session_key'], chat_history)
@@ -61,16 +76,13 @@ def app():
         for chat in chat_history:
             role = chat["role"]
             message = chat["message"]
-            with st.chat_message(role):
-                st.write(message)
+            with st.container():
+                st.write(f"{role}: {message}")
+
     else:
         st.warning("Please create a new chat or select an existing chat.")
 
-def load_chat_history(session_key):
-    # (unchanged code)
-
-def save_chat_history(session_key, chat_history):
-    # (unchanged code)
+# Rest of the code remains the same...
 
 if __name__ == "__main__":
     app()
