de490ed17bfa319f6540abb9e14a86e7e0d97a9f
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-25 13:32:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,42 @@
 import streamlit as st
 import requests
 
-# Function to fetch model options dynamically
-def get_available_models():
-    api_endpoint = "https://your-server-url/models"  # Replace with your model server's endpoint
-    response = requests.get(api_endpoint)
-    models = response.json()
-    return models
+# Server URL
+SERVER_URL = "http://localhost:4000"
 
-# API call function with error handling
-def make_api_call(server_url, model, messages):
-    api_endpoint = f"{server_url}/chat/completions"
+# Sample API call function
+def make_api_call(model, messages):
+    api_endpoint = f"{SERVER_URL}/chat/completions"
     data = {"model": model, "messages": messages}
-    try:
-        response = requests.post(api_endpoint, json=data)
-        response.raise_for_status()  # Raise an exception for non-200 status codes
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        st.error(f"API call failed: {e}")
-        return None
+    response = requests.post(api_endpoint, json=data)
+    return response.json()
 
-# Streamlit app with enhanced features
+# Streamlit app with sidebar for custom data input
 def main():
     st.title("LLM Playground")
 
-    # Sidebar
+    # Sidebar for custom data input
     st.sidebar.title("Custom Data Input")
+    model = st.sidebar.selectbox("Select Model", ["gpt-3.5-turbo", "command-nightly", "j2-mid"])
 
-    server_url = st.sidebar.text_input("Enter Server URL", "http://localhost:4000")
-
-    # Fetch available models dynamically
-    available_models = get_available_models()
-    model = st.sidebar.selectbox("Select Model", available_models)
-
-    # Message input with improved layout
-    st.sidebar.subheader("Messages")
+    # Allow users to add multiple messages
     messages = []
-    for i in range(1, 11):
-        role_input = st.sidebar.selectbox(f"Role for Message {i}", ["user", "assistant"], key=f"role_{i}")
-        content_input = st.sidebar.text_area(f"Content for Message {i}", key=f"content_{i}")
-        messages.append({"role": role_input, "content": content_input})
+    message_count = st.sidebar.number_input("Number of Messages", value=1, min_value=1, max_value=10)
+    for i in range(message_count):
+        role = st.sidebar.selectbox(f"Role for Message {i+1}", ["user", "assistant"])
+        content = st.sidebar.text_area(f"Content for Message {i+1}")
+        messages.append({"role": role, "content": content})
 
-    # Preview and API call
+    # Streamlit main content
     st.subheader("Preview Messages")
     st.json(messages)
 
+    # Call API button
     if st.button("Call API"):
         st.info("Calling API...")
-        response = make_api_call(server_url, model, messages)
-        if response:
-            st.success("API call successful!")
-            st.json(response)
+        response = make_api_call(model, messages)
+        st.success("API call successful!")
+        st.json(response)
 
 if __name__ == "__main__":
     main()
